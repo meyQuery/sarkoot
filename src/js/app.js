9e3322ce5a6dd2c0cea63261d6b7986b09aa8415
baravak <itb.baravak@gmail.com>
2020-03-19 19:11:31 +0330

    check if am4core

--- src/js/app.js
+++ src/js/app.js
@@ -28,89 +28,91 @@ if (profile)
     });
 }
 
+if (am4core)
+{
+    am4core.ready(function () {
 
-am4core.ready(function () {
+        // Themes begin
+        am4core.useTheme(am4themes_amcharts);
+        // Themes end
 
-    // Themes begin
-    am4core.useTheme(am4themes_amcharts);
-    // Themes end
+        // Create chart instance
+        var chart = am4core.create("chartdiv", am4charts.XYChart);
 
-    // Create chart instance
-    var chart = am4core.create("chartdiv", am4charts.XYChart);
+        chart.rtl = true;
 
-    chart.rtl = true;
+        // Add data
+        chart.data = [{
+            "country": "USA",
+            "visits": 2025
+        }, {
+            "country": "China",
+            "visits": 1882
+        }, {
+            "country": "Japan",
+            "visits": 1809
+        }, {
+            "country": "Germany",
+            "visits": 1322
+        }, {
+            "country": "UK",
+            "visits": 1122
+        }, {
+            "country": "France",
+            "visits": 1114
+        }, {
+            "country": "India",
+            "visits": 984
+        }, {
+            "country": "Spain",
+            "visits": 711
+        }, {
+            "country": "Netherlands",
+            "visits": 665
+        }, {
+            "country": "Russia",
+            "visits": 580
+        }, {
+            "country": "South Korea",
+            "visits": 443
+        }, {
+            "country": "Canada",
+            "visits": 441
+        }, {
+            "country": "Brazil",
+            "visits": 395
+        }];
 
-    // Add data
-    chart.data = [{
-        "country": "USA",
-        "visits": 2025
-    }, {
-        "country": "China",
-        "visits": 1882
-    }, {
-        "country": "Japan",
-        "visits": 1809
-    }, {
-        "country": "Germany",
-        "visits": 1322
-    }, {
-        "country": "UK",
-        "visits": 1122
-    }, {
-        "country": "France",
-        "visits": 1114
-    }, {
-        "country": "India",
-        "visits": 984
-    }, {
-        "country": "Spain",
-        "visits": 711
-    }, {
-        "country": "Netherlands",
-        "visits": 665
-    }, {
-        "country": "Russia",
-        "visits": 580
-    }, {
-        "country": "South Korea",
-        "visits": 443
-    }, {
-        "country": "Canada",
-        "visits": 441
-    }, {
-        "country": "Brazil",
-        "visits": 395
-    }];
+        // Create axes
 
-    // Create axes
+        var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+        categoryAxis.dataFields.category = "country";
+        categoryAxis.renderer.grid.template.location = 0;
+        categoryAxis.renderer.minGridDistance = 30;
 
-    var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-    categoryAxis.dataFields.category = "country";
-    categoryAxis.renderer.grid.template.location = 0;
-    categoryAxis.renderer.minGridDistance = 30;
+        categoryAxis.renderer.labels.template.adapter.add("dy", function (dy, target) {
+            if (target.dataItem && target.dataItem.index & 2 == 2) {
+                return dy + 25;
+            }
+            return dy;
+        });
 
-    categoryAxis.renderer.labels.template.adapter.add("dy", function (dy, target) {
-        if (target.dataItem && target.dataItem.index & 2 == 2) {
-            return dy + 25;
-        }
-        return dy;
+        var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+        // Create series
+        var series = chart.series.push(new am4charts.ColumnSeries());
+        series.dataFields.valueY = "visits";
+        series.dataFields.categoryX = "country";
+        series.name = "Visits";
+        series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+        series.columns.template.fillOpacity = .8;
+
+        var columnTemplate = series.columns.template;
+        columnTemplate.strokeWidth = 2;
+        columnTemplate.strokeOpacity = 1;
+
     });
-
-    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-
-    // Create series
-    var series = chart.series.push(new am4charts.ColumnSeries());
-    series.dataFields.valueY = "visits";
-    series.dataFields.categoryX = "country";
-    series.name = "Visits";
-    series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
-    series.columns.template.fillOpacity = .8;
-
-    var columnTemplate = series.columns.template;
-    columnTemplate.strokeWidth = 2;
-    columnTemplate.strokeOpacity = 1;
-
-}); // end am4core.ready()
+}
 
 $(document).ready(function() {
     $('.select2').select2();
