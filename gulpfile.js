d7f7c696adaee9be0a248157c577d469ffe2538c
baravak <itb.baravak@gmail.com>
2020-03-22 07:16:16 +0430

    gulpfile change

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass');
 const minifyCSS = require('gulp-csso');
+const minify = require('gulp-minify');
 const concat = require('gulp-concat');
 
 function css() {
@@ -12,13 +13,20 @@ function css() {
 
 function js() {
     return src('src/js/**/*.js', {sourcemaps: true})
-        .pipe(concat('sarkoot.min.js'))
-        .pipe(dest('dist/js', { sourcemaps: true }))
+        .pipe(concat('sarkoot.js'))
+        .pipe(minify({
+            ext: {
+                min: '.min.js'
+            },
+        }))
+        .pipe(dest('dist/js', { sourcemaps: false }))
 }
-
-exports.js = js;
-exports.css = css;
-exports.default = function() {
+function watchFile()
+{
     watch('src/scss/**/*.scss', css);
     watch('src/js/**/*.js', js);
 }
+exports.js = js;
+exports.css = css;
+exports.watchFile = watchFile;
+exports.default = parallel(js, css, watchFile);
